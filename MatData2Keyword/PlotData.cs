be732d4071f1b26952e184033a6b1dae21c2ba98
Cesar A. Rivas <crivasg@gmail.com>
2013-04-13 18:48:53 -0400

    Add references to the MS Charting, modify the constructor of the PlotData class

--- MatData2Keyword/PlotData.cs
+++ MatData2Keyword/PlotData.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing;
 
 namespace MatData2Keyword
 {
     class PlotData
     {
 
-        public PlotData()
+        public PlotData(List<TestData> testData, Chart chart)
         { 
         
         }
