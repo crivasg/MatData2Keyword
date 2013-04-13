27132e78bc9ecb0bd6100072c498e95550ac45cf
Cesar A. Rivas <crivasg@gmail.com>
2013-04-13 15:50:32 -0400

    Changed the definition of the constructor of the CSVRead class; added Accessors to the CSVRead class

--- MatData2Keyword/CSVRead.cs
+++ MatData2Keyword/CSVRead.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace MatData2Keyword
 {
     class CSVRead
     {
 
-        public CSVRead()
+        public String[] Headers { private set; get; }
+        public String[] SubHeaders { private set; get; }
+        public float[,] Data { private set; get; }
+        public int NumColumns { private set; get; }
+        public int NumLines { private set; get; }
+
+        public CSVRead(StreamReader fileData)
         {
         
         }
