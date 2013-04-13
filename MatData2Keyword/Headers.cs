85f361487ea3d990869d059ab4a807af69812502
Cesar A. Rivas <crivasg@gmail.com>
2013-04-13 19:24:16 -0400

    Implemented the Headers class

--- MatData2Keyword/Headers.cs
+++ MatData2Keyword/Headers.cs
@@ -7,5 +7,27 @@ namespace MatData2Keyword
 {
     class Headers
     {
+
+        public String[] Titles { set; private get; }
+        public String[] Units { set; private get;}
+
+        public String this[TestDataIndices index]
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(this.Units[(int)index]))
+                {
+                    return String.Copy(this.Titles[(int)index]);   
+                }
+                return String.Concat(this.Titles[(int)index], " ", this.Units[(int)index]);
+            }
+            
+        }
+
+        public Headers()
+        { 
+        
+        }
+
     }
 }
