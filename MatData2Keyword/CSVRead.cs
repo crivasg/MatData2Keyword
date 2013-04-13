714752f523787a906f301b3c0310abaa46541117
Cesar A. Rivas <crivasg@gmail.com>
2013-04-13 17:42:40 -0400

    Read all of the CSV file and store it in the member Data

--- MatData2Keyword/CSVRead.cs
+++ MatData2Keyword/CSVRead.cs
@@ -11,7 +11,7 @@ namespace MatData2Keyword
 
         public String[] Headers { private set; get; }
         public String[] SubHeaders { private set; get; }  // subheaders contains the units.
-        public float[,] Data { private set; get; }
+        public double[,] Data { private set; get; }
         public int NumColumns { private set; get; }
         public int NumLines { private set; get; }
 
@@ -19,6 +19,10 @@ namespace MatData2Keyword
 
         public CSVRead(StreamReader fileData)
         {
+            this.NumColumns = 0;
+            this.NumLines = 0;
+
+            bool hasUnits = false;
 
             String temp = fileData.ReadLine();
             this.Headers = temp.Split(delimiterChars);
@@ -29,11 +33,49 @@ namespace MatData2Keyword
             temp = fileData.ReadLine();
             String[] aux = temp.Split(delimiterChars);
             double myDouble = 0.0;
-            bool status = double.TryParse(aux[0], out myDouble);
-
+            bool status = double.TryParse(aux[0], out myDouble);  // check if the 2nd line contains
+                                                                  // a string or a double. 
             if (!status)
             {
                 SubHeaders = temp.Split(delimiterChars);
+                hasUnits = true;
+            }
+            else
+            {
+                ++this.NumLines;
+            }
+
+            while( (temp = fileData.ReadLine() ) != null )
+            {
+                if (temp.Length > 0)
+                {
+                    ++this.NumLines;
+                }
+            }
+
+            // Init the data array.
+            this.Data = new double[this.NumLines, this.NumColumns];
+            fileData.BaseStream.Seek(0,0);
+
+            temp = fileData.ReadLine();
+            if(hasUnits)
+            {
+                temp = fileData.ReadLine();
+            }
+
+            for ( int i = 0 ; i < this.NumLines ; ++i )
+            {
+                temp = fileData.ReadLine();
+                aux = temp.Split(delimiterChars);
+                for (int j = 0; j < this.NumColumns; ++j )
+                {
+                    double tmpDouble = 0.0;
+                    bool try2parse = double.TryParse(aux[j], out tmpDouble);
+                    if (try2parse)
+                    {
+                        this.Data[i, j] = tmpDouble;
+                    }
+                }
             }
             
 
