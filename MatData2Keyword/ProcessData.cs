ab59a80c6bcb0a0608d834c17fb3697c8bffd64f
Cesar A. Rivas <crivasg@gmail.com>
2013-04-14 20:31:44 -0400

    Added references for the chart object. The constructor has a chart object as a parameter

--- MatData2Keyword/ProcessData.cs
+++ MatData2Keyword/ProcessData.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing;
+
+
 namespace MatData2Keyword
 {
     class ProcessData
     {
 
-        public ProcessData()
+        public ProcessData( Chart chart)
         { 
         
         }
