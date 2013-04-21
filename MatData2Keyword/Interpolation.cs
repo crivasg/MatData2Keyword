f7b8250721424e1f642e6a1aaa9535569c6c4a53
Cesar A. Rivas <crivasg@gmail.com>
2013-04-21 13:50:31 -0500

    Implemented the public method Linear, to perform a linear interpolation of the chart at xValue

--- MatData2Keyword/Interpolation.cs
+++ MatData2Keyword/Interpolation.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Windows.Forms.DataVisualization;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
 namespace MatData2Keyword
 {
     public static class Interpolation
     {
+        public static double Linear( Chart chart, double xValue )
+        {
+            double yValue = 0.0;
+
+            return yValue;
+        }
     }
 }
