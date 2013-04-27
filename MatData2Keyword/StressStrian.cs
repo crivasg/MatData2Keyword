68be14c0be664a4078f8d218025254f4312a0a92
Cesar A. Rivas <crivasg@gmail.com>
2013-04-27 17:51:31 -0500

    Added to the project a StressStrain class

---
+++ MatData2Keyword/StressStrian.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatData2Keyword
+{
+    class StressStrian
+    {
+        public double Strain { get; set; }
+        public double Stress { get; set; }
+
+        public StressStrian()
+        { 
+        
+        }
+
+    }
+}
