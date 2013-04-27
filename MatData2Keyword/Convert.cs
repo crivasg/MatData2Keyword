bce9b9c71dabc67051bd9a0ba5b3317d4ee9c93d
Cesar A. Rivas <crivasg@gmail.com>
2013-04-27 17:31:04 -0500

    Added a Convert class to get the True data from the engineering test data

---
+++ MatData2Keyword/Convert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatData2Keyword
+{
+    class Convert
+    {
+        public List<TestData> EngineeringData { set; private get; }
+        public List<TestData> TrueData { set; private get; }
+
+        public Convert()
+        { 
+        
+        }
+
+    }
+}
