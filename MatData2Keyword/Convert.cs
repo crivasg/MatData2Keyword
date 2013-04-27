3940806c50b4564d880da2595c8601b1ba81a78e
Cesar A. Rivas <crivasg@gmail.com>
2013-04-27 17:51:07 -0500

    Added a the true data class member

--- MatData2Keyword/Convert.cs
+++ MatData2Keyword/Convert.cs
@@ -8,9 +8,14 @@ namespace MatData2Keyword
     class Convert
     {
         public List<TestData> EngineeringData { set; private get; }
-        public List<TestData> TrueData { set; private get; }
+        public List<TestData> TrueData { private set; get; }
 
         public Convert()
+        {
+            this.TrueData = new List<TestData>();
+        }
+
+        public void Run()
         { 
         
         }
