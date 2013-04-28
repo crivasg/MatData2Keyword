a995d23c83798a8c9377c3697f7b95b4b13a4a6a
Cesar A. Rivas <crivasg@gmail.com>
2013-04-27 22:07:04 -0500

    Added a static class to find the intersectation of two chart series

---
+++ MatData2Keyword/Intersection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms.DataVisualization;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MatData2Keyword
+{
+    public static class Intersection
+    {
+        public static DataPoint TwoSeries(Series series1, Series series2)
+        {
+            DataPoint dataPoint = null;
+
+
+
+            return dataPoint;
+        }
+    }
+}
