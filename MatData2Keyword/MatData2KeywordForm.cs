b8d6efb08d3f4ae8400b98c1aebd292cd68c059a
Cesar A. Rivas <crivasg@gmail.com>
2013-04-27 21:17:53 -0500

    Commented out unneeded code, which was used to test some data

--- MatData2Keyword/MatData2KeywordForm.cs
+++ MatData2Keyword/MatData2KeywordForm.cs
@@ -135,13 +135,13 @@ namespace MatData2Keyword
                     yMax = maxDataPointY.YValues[0];
                 }
 
-                double yy = Interpolation.Linear(s,100.0);
-                yy = Interpolation.Linear(s, -100.0);
-                yy = Interpolation.Linear(s, 0.001);
+                //double yy = Interpolation.Linear(s,100.0);
+                //yy = Interpolation.Linear(s, -100.0);
+                //yy = Interpolation.Linear(s, 0.001);
 
             }
 
-            MessageBox.Show(String.Format(@"{0} {1}",xMax,yMax));
+            //MessageBox.Show(String.Format(@"{0} {1}",xMax,yMax));
         }
 
         private void AddTwoPercentOffset()
